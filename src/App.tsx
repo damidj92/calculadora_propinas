63758233df34744e0e4595c7131eb10e3471174b
damidj92 <damidj92@gmail.com>
2025-04-20 21:34:43 -0300

    creando menuItem component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import MenuItem from "./components/MenuItem.tsx"
+import { menuItems } from "./data/db.ts"
+
 function App() {
 
   return (
@@ -5,6 +8,20 @@ function App() {
       <header className="bg-teal-400 py-5">
         <h1 className="text-center text-4xl font-black">Calculadora de propinas y consumo</h1>
       </header>
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div>
+          <h2 className="">Menú</h2>
+          {menuItems.map((item) => (
+            <MenuItem 
+              key={item.id}
+            />
+          ))}
+        </div>
+        
+        <div>
+          <h2 className="">Consumo</h2>
+        </div>
+      </main>
     </>
   )
 }
