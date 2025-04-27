b28c0ed9c411c3ba9d0c2ba136c6586307ccf8b1
damidj92 <damidj92@gmail.com>
2025-04-27 18:42:01 -0300

    mostrando menu items

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,16 @@ function App() {
         <h1 className="text-center text-4xl font-black">Calculadora de propinas y consumo</h1>
       </header>
       <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
-        <div>
-          <h2 className="">Menú</h2>
-          {menuItems.map((item) => (
-            <MenuItem 
-              key={item.id}
-            />
-          ))}
+        <div className="p-5">
+          <h2 className="text-4xl font-black">Menú</h2>
+          <div className="space-y-3 mt-10">
+            {menuItems.map((item) => (
+              <MenuItem 
+                key={item.id}
+                item={item}
+              />
+            ))}
+          </div>
         </div>
         
         <div>
